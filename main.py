45c079e885ebb23f3a7120cace8892592c69327a
Mateusz Stankiewicz <mstankiewicz@egnyte.com>
2021-02-26 12:27:30 +0100

    reformatting

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = "uploads"
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.jpeg']
+app.config["UPLOAD_FOLDER"] = "uploads"
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+app.config["UPLOAD_EXTENSIONS"] = [".jpg", ".png", ".jpeg"]
 
 
 @app.route("/upload-image", methods=["GET", "POST"])
